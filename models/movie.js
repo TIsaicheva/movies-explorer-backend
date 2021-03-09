8b5f4af579d3852bad75d737bfa0d8e7322394fe
Tatiana Isaicheva <mujer-amante@yandex.ru>
2021-03-09 13:25:18 +0300

    fix: add default values into movies schema

--- models/movie.js
+++ models/movie.js
@@ -1,14 +1,21 @@
 const mongoose = require('mongoose');
 const { NOT_CORRECT_FORMAT_MESSAGE } = require('../utils/constants');
 
+function currentYear() {
+  const year = new Date().getFullYear();
+  return year;
+}
+
 const movieSchema = new mongoose.Schema({
   country: {
     type: String,
     required: true,
+    default: 'США',
   },
   director: {
     type: String,
     required: true,
+    default: 'Режиссёр',
   },
   duration: {
     type: Number,
@@ -17,14 +24,17 @@ const movieSchema = new mongoose.Schema({
   year: {
     type: String,
     required: true,
+    default: currentYear,
   },
   description: {
     type: String,
     required: true,
+    default: 'Фильм',
   },
   image: {
     type: String,
     required: true,
+    default: 'https://images.puella-magi.net/thumb/2/27/No_Image_Wide.svg/1600px-No_Image_Wide.svg.png?20110202071158',
     validate: {
       validator: (v) => {
         const regExp = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w\W.-]*)#?$/g;
@@ -36,6 +46,7 @@ const movieSchema = new mongoose.Schema({
   trailer: {
     type: String,
     required: true,
+    default: 'https://www.youtube.com',
     validate: {
       validator: (v) => {
         const regExp = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w\W.-]*)#?$/g;
@@ -47,6 +58,7 @@ const movieSchema = new mongoose.Schema({
   thumbnail: {
     type: String,
     required: true,
+    default: 'https://images.puella-magi.net/thumb/2/27/No_Image_Wide.svg/1600px-No_Image_Wide.svg.png?20110202071158',
     validate: {
       validator: (v) => {
         const regExp = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w\W.-]*)#?$/g;
